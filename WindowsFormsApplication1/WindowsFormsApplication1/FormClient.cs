8f3c042c3b59c60daf0447113be07e0fad3fb369
maxim <max722010@gmail.com>
2018-03-08 22:46:42 +0500

    создание протатипа формы клиента и статуса

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/FormClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FormClient : Form
+    {
+        public FormClient()
+        {
+            InitializeComponent();
+        }
+
+        private void butAddCancel_Click(object sender, EventArgs e)
+        {
+            groupAdd.Visible = false;
+        }
+
+        private void butAdd_Click(object sender, EventArgs e)
+        {
+            groupAdd.Visible = true;
+        }
+
+        private void butEdit_Click(object sender, EventArgs e)
+        {
+            groupEdit.Visible = true; 
+        }
+
+        private void butEditCancel_Click(object sender, EventArgs e)
+        {
+            groupEdit.Visible = false;
+        }
+    }
+}

706896ea88638b1d1d089ca5ae86b6dd9f0024e4
gachiBass <danich1396@gmail.com>
2018-03-10 23:40:26 +0500

    op hyi

---
+++ Salon/Salon/Authorization.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Salon
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+
+        public Authorization()
+        {
+            InitializeComponent();           
+        }
+
+        public string Connection()
+        {
+            /*DATA SOURCE менять самому*/
+            string con = @"Data Source=DESKTOP-H5176PR;Initial Catalog=Task3;Integrated Security=True";
+            return con;
+        }
+        public string Connection(string login, string pass)
+        {
+            /*DATA SOURCE менять самому*/
+            string con = @"Data Source=DESKTOP-H5176PR;Initial Catalog=Task3;User Id=" + login + ";  Password=" + pass + ";";
+            return con;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Authorization_();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Registration();
+        }
+
+        public void Authorization_()
+        {
+            string connectionStr = Connection(textBox.Text, textBox1.Text);
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection(connectionStr);
+                con.Open();
+                if (con != null)
+                { MessageBox.Show("Prohodi STALKER"); }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (con != null)
+                    con.Close();
+            }
+        }
+        public void Registration()
+        {
+            string connectionStr = Connection();
+            SqlConnection con = null;
+            SqlCommand com = null;
+            try
+            {
+                con = new SqlConnection(connectionStr);
+                con.Open();
+                string sql = @"USE master "+
+"create login "+textBox.Text+@" with password = '"+textBox1.Text+ @"', check_policy = off;
+USE[Task3]
+create user " + textBox.Text + @" from login " + textBox.Text + @";
+exec sp_addrolemember 'db_datareader', '" + textBox.Text + "'";                
+                if (con != null)
+                {
+                    com = con.CreateCommand();
+                    com.CommandText = sql;
+                    com.ExecuteNonQuery();
+                    MessageBox.Show("Gotovo");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (con != null)
+                    con.Close();
+            }
+        }
+    }
+}
---
+++ Salon/Salon/Users.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Salon
+{
+    /// <summary>
+    /// Логика взаимодействия для Users.xaml
+    /// </summary>
+    public partial class Users : Window
+    {
+        public Users()
+        {
+            InitializeComponent();
+        }
+        public string Connection()
+        {
+            /*DATA SOURCE менять самому*/
+            string con = @"Data Source=DESKTOP-H5176PR;Initial Catalog=Task3;Integrated Security=True";
+            return con;
+        }
+
+        public void OnLoad(object sender, RoutedEventArgs e)
+        {
+            string str = "use Task3 select name from sys.database_principals where [type] <> 'r' and [name] not in ( 'dbo', 'sys', 'INFORMATION_SCHEMA') order by name";
+            DataTable dt = ZaprosList(str);
+            foreach (DataRow dr in dt.Rows)
+            {
+                listBox.Items.Add(dr["name"].ToString());
+            }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (listBox.Items.Count != 0)
+            {
+                string str = "use Task3 EXEC sp_helpuser '"+listBox.SelectedValue+"'";
+                DataTable dt = ZaprosList(str);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    listBox1.Items.Add(dr["RoleName"].ToString());
+                }
+            }
+        }
+        public DataTable ZaprosList(string zapros)
+        {
+            string connectionStr = Connection();
+            SqlConnection con = null;
+            SqlCommand com = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                con = new SqlConnection(connectionStr);
+                con.Open();
+                if (con != null)
+                {
+                    com = con.CreateCommand();
+                    com.CommandText = zapros;
+                    SqlDataAdapter adap = new SqlDataAdapter(com);
+                    SqlCommandBuilder bild = new SqlCommandBuilder(adap);
+                    adap.Fill(dt);
+                    return dt;
+                }
+                else
+                { return dt; }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return dt;
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //AddRole();
+            Osnova.Visibility = Visibility.Hidden;
+        }
+        public void AddRole1()
+        {
+            //listBox2.Items.Clear();
+            //List<string> user = new List<string>();
+            //List<string> db = new List<string>();
+            //string str = "use Task3 EXEC sp_helpuser '" + listBox.SelectedValue + "'";
+            //DataTable dt = ZaprosList(str);
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    user.Add(dr["RoleName"].ToString());
+            //}
+            //string str1 = "use Task3 EXEC sp_helprole";
+            //DataTable dt1 = ZaprosList(str1);
+            //foreach (DataRow dr in dt1.Rows)
+            //{
+            //    db.Add(dr["RoleName"].ToString());
+            //}
+            //List<string> NoUserRole = db.Except(user).ToList();
+            //foreach (var no in NoUserRole)
+            //{
+            //    listBox2.Items.Add(no.ToString());
+            //}
+            //listBox2.Visibility = Visibility.Visible;
+        }
+        public void LoseFocus(object sender, RoutedEventArgs e)
+        {
+            //listBox2.Visibility = Visibility.Hidden;
+        }
+    }
+}

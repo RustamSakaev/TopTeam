ed57b19a51d4e8743baeacb5cd523676bcc44e31
frammmm <frammm1337@gmail.com>
2018-03-12 23:57:00 +0500

    Status form

--- Salon/Salon/StatusForm.xaml.cs
+++ Salon/Salon/StatusForm.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,59 @@ namespace Salon
     /// </summary>
     public partial class StatusForm : Window
     {
+        private readonly string[] _hiddenFields = { "id" };
+        private DataTable _currentFormData = new DataTable();
+
+        private DataTable CurrentFormData
+        {
+            get { return _currentFormData; }
+            set { _currentFormData = value; StatusGrid.DataContext = _currentFormData; }
+        }
+
         public StatusForm()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CurrentFormData = DBStatus.GetStatuses();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var form = new StatusActionForm(new Action(() => { CurrentFormData = DBStatus.GetStatuses(); }), FormState.Add);
+            form.ShowDialog();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var idx = ((DataTable)StatusGrid.DataContext).Columns.IndexOf("id");
+            var id = ((DataRowView)StatusGrid.SelectedItem)?.Row[idx].ToString();
+
+            if (id is null) return;
+
+            var form = new StatusActionForm(new Action(() => { CurrentFormData = DBStatus.GetStatuses(); ; }), FormState.Edit, id);
+            form.ShowDialog();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var idx = ((DataTable)StatusGrid.DataContext).Columns.IndexOf("id");
+
+            foreach (DataRowView selectedItem in StatusGrid.SelectedItems)
+            {
+                DBStatus.DeleteStatus(selectedItem.Row[idx].ToString());
+            }
+
+            CurrentFormData = DBStatus.GetStatuses();
+        }
+
+        private void StatusGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (!_hiddenFields?.Contains(e.PropertyName) == true) return;
+
+            e.Cancel = true;
         }
     }
 }

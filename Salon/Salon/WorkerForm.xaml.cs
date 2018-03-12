dad0505899e4c240eb0530521b47da3783cc92a1
virtes775 <2010pelmen2010@gmail.com>
2018-03-12 23:05:40 +0500

    worker func

--- Salon/Salon/WorkerForm.xaml.cs
+++ Salon/Salon/WorkerForm.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,16 @@ namespace Salon
         public WorkerForm()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataTable result = DBWorker.GetWorkers();
+
+            WorkersGrid.DataContext = result.DefaultView;
+
+
+
         }
     }
 }

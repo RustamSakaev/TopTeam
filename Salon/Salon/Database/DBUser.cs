df52cdd9645cbbb69c616a968054344be0c7e46a
gachiBass <danich1396@gmail.com>
2018-03-13 14:42:05 +0500

    ++

---
+++ Salon/Salon/Database/DBUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Salon
+{
+    internal static class DBUser
+    {
+        public static void CreateUser(string login, string pass)
+        {
+            var command = new SqlCommand
+            {
+                CommandText = $@"USE master
+                    create login @login with password = '@pass', check_policy = off;
+                    USE[Task3]
+                    create user @login from login @login
+                    exec sp_addrolemember 'db_datareader', '@login'"
+            };       
+            command.Parameters.AddWithValue("@login", login);
+            command.Parameters.AddWithValue("@pass", pass);
+            DBCore.ExecuteCommand(command);
+        }
+        public static void Connection(string server, string login, string pass)
+        {
+            DBCore.InitLogPass(server,login, pass);
+        }
+        public static void ChangePass(string user, string newpass, string oldpass)
+        {
+            var command = new SqlCommand
+            {
+                CommandText = $@"ALTER LOGIN @login WITH PASSWORD = '@newpass' OLD_PASSWORD = '@oldpass'"
+            };
+
+            command.Parameters.AddWithValue("@login", user);
+            command.Parameters.AddWithValue("@newpass", newpass);
+            command.Parameters.AddWithValue("@oldpass", oldpass);
+
+            DBCore.ExecuteCommand(command);
+        }
+    }
+}
--- Salon/Salon/Kabinet.xaml.cs
+++ Salon/Salon/Kabinet.xaml.cs
@@ -30,44 +30,10 @@ namespace Salon
             get { return userName; }
             set { userName = value; }
         }
-        public string Connection()
-        {
-            /*DATA SOURCE менять самому*/
-            string con = @"Data Source=DESKTOP-H5176PR;Initial Catalog=Task3;Integrated Security=True";
-            return con;
-        }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            ChangePass(OldPassBox.Text,NewPassBox.Text);
-        }
-
-        public void ChangePass(string oldP,string newP)
-        {
-            string connectionStr = Connection();
-            SqlConnection con = null;
-            SqlCommand com = null;
-            try
-            {
-                con = new SqlConnection(connectionStr);
-                con.Open();
-                string sql = "ALTER LOGIN "+userName+" WITH PASSWORD = '"+newP+"' OLD_PASSWORD = '"+oldP+"'";
-                if (con != null)
-                {
-                    com = con.CreateCommand();
-                    com.CommandText = sql;
-                    com.ExecuteNonQuery();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                if (con != null)
-                    con.Close();
-            }
+            DBUser.ChangePass(userName, NewPassBox.Text, OldPassBox.Text);
         }
     }
 }

e217f4e5d35fef9b69624c04eb6057d92a60e61c
frammmm <frammm1337@gmail.com>
2018-03-16 00:10:20 +0500

    Visit form filters

--- Salon/Salon/VisitForm.xaml.cs
+++ Salon/Salon/VisitForm.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using Salon.Misc;
 
 namespace Salon
 {
@@ -12,8 +14,8 @@ namespace Salon
     public partial class VisitForm : Window
     {
         private readonly string[] _hiddenFields = { "id", "clientid", "workerid", "statusid" };
+        private readonly List<Filter> _currentFilters = new List<Filter>();
         private DataTable _currentFormData = new DataTable();
-        private string _currentFilter = "";
 
         private DataTable CurrentFormData
         {
@@ -21,17 +23,33 @@ namespace Salon
             set { _currentFormData = value; VisitGrid.DataContext = _currentFormData; }
         }
 
-        private string CurrentFilter
+        private void SetFilter(string key, string column, string expression)
         {
-            get { return _currentFilter; }
-            set { _currentFilter = value; DisplayDataWithFilter(); }
+            var filterIdx = _currentFilters.FindIndex(filter => filter.InnerKey.Equals(key));
+            var newFilter = new Filter(key, column, expression);
+
+
+            if (filterIdx == -1) _currentFilters.Add(newFilter);
+            else _currentFilters[filterIdx] = newFilter;
+
+            DisplayDataWithFilters();
         }
 
-        //TODO Implement advanced filters
-        private void DisplayDataWithFilter()
+        private void RemoveFilter(string key)
+        {
+            _currentFilters.RemoveAll(filter => filter.InnerKey.Equals(key));
+
+            DisplayDataWithFilters();
+        }
+
+        private void DisplayDataWithFilters()
         {
             var displayData = _currentFormData.DefaultView;
-            displayData.RowFilter = $"Клиент LIKE '%{_currentFilter}%' OR Сотрудник LIKE '%{_currentFilter}%'";
+
+            var filters = string.Join(" AND ",
+                _currentFilters.Select(filter => $"{filter.Key} {filter.Expression}"));
+
+            displayData.RowFilter = filters;
 
             VisitGrid.DataContext = displayData;
         }
@@ -39,22 +57,72 @@ namespace Salon
         public VisitForm()
         {
             InitializeComponent();
-
-            ClientCmbBox.ItemsSource = DBClient.GetClients().DefaultView;
-            ClientCmbBox.DisplayMemberPath = "ФИО";
-            ClientCmbBox.SelectedValuePath = "id";
-
-            WorkerCmbBox.ItemsSource = DBWorker.GetWorkers().DefaultView;
-            WorkerCmbBox.DisplayMemberPath = "ФИО";
-            WorkerCmbBox.SelectedValuePath = "id";
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             CurrentFormData = DBVisit.GetVisits();
+
+            ClientCmbBox.Items.Add("Все");
+            ClientCmbBox.SelectedItem = "Все";
+            foreach (DataRow client in DBClient.GetClients().Rows)
+            {
+                ClientCmbBox.Items.Add(client["ФИО"]);
+            }
+
+            WorkerCmbBox.Items.Add("Все");
+            WorkerCmbBox.SelectedItem = "Все";
+            foreach (DataRow worker in DBWorker.GetWorkers().Rows)
+            {
+                WorkerCmbBox.Items.Add(worker["ФИО"]);
+            }
         }
 
-        
+        private void ClientCmbBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedClient = e.AddedItems[0].ToString();
+            var filterValue = selectedClient != "Все" ? selectedClient : string.Empty;
+
+            SetFilter("Client", "Клиент", $"LIKE '%{filterValue}%'");
+        }
+
+        private void WorkerCmbBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedWorker = e.AddedItems[0].ToString();
+            var filterValue = selectedWorker != "Все" ? selectedWorker : string.Empty;
+
+            SetFilter("Worker", "Сотрудник", $"LIKE '%{filterValue}%'");
+        }
+
+        private void StartDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            const string key = "DateFrom";
+
+            if (e.AddedItems.Count <= 0)
+            {
+                RemoveFilter(key);
+                return;
+            }
+
+            DateTime.TryParse(e.AddedItems[0].ToString(), out var selectedDate);
+
+            SetFilter(key, "[Дата посещения]", $">= #{selectedDate:MM/dd/yyyy}#");
+        }
+
+        private void EndDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            const string key = "DateTo";
+
+            if (e.AddedItems.Count <= 0)
+            {
+                RemoveFilter(key);
+                return;
+            }
+
+            DateTime.TryParse(e.AddedItems[0].ToString(), out var selectedDate);
+
+            SetFilter(key, "[Дата посещения]", $"<= #{selectedDate:MM/dd/yyyy}#");
+        }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
@@ -97,7 +165,15 @@ namespace Salon
 
         private void VisitGrid_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            DisplayDataWithFilter();
+            DisplayDataWithFilters();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClientCmbBox.SelectedItem = "Все";
+            WorkerCmbBox.SelectedItem = "Все";
+            StartDatePicker.SelectedDate = null;
+            EndDatePicker.SelectedDate = null;
         }
     }
 }
